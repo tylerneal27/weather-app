f59cb9c179f07da4364f6bf44d50408b3572a29e
Tyler Neal <will1neal27@gmail.com>
2022-08-14 15:00:06 -0400

    added framework for html added fetch calls to js

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+// when a city is searched the user in presented with weather stats
+    // city name and date and an icon for weather conditions
+    // uv index, temp, humidity, wind speed
+    // uv index will have color for favorable, moderate, or severe weather
+    // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity 
+// when cities in the history are clicked that cities weather appears again        
+// when city is searched for put in local storage and make appear in history 
+    // last searched city will pop up on the page
+var apiKey = '843fa40ad68a96668befb0da86d9b44b'
+var cityInput = document.getElementById('cityInputs')
+var citySearch = document.getElementById('citySearch')
+
+function weatherData(lat,lon,name) {
+var url = 'https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey
+
+fetch(url)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    var cityName = document.getElementById('cityName')
+    var temp = document.getElementById('temp')
+    cityName.textContent = name
+    temp.textContent = data.current.temp
+})
+}
+function locationData() {
+    var cityName = cityInput.value
+var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=' + apiKey
+
+fetch(url)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    var lat = data [0].lat
+    var lon = data [0].lon
+    var name = data [0].name
+    weatherData(lat,lon,name)
+    
+})
+}
+
+
+
+citySearch.addEventListener('click', locationData)
